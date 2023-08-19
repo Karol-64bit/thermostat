b0820323fd001f1d9847ea3a84023bf110b1ba54
Karol <67333775+Karol-64bit@users.noreply.github.com>
2023-08-19 21:09:06 +0200

    init

---
+++ Arduino/Projekt_code/Projekt_code.ino
@@ -0,0 +1,83 @@
+#include <ArduinoWiFiServer.h>
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiGratuitous.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+// #include <WiFi.h>
+#include "Libraries/TokenHelper.h"
+#include "Libraries/RTDBHelper.h"
+
+#include "config.h"
+
+#include <Firebase_ESP_Client.h>
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+bool signupOK = false;
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println();
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+    /* Sign up */
+  if (Firebase.signUp(&config, &auth, API_KEY, "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    Serial.printf(API_KEY);
+  }
+
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  
+}
+
+void loop(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    if(Firebase.RTDB.setInt(&fbdo,"Test/data", sendDataPrevMillis)){
+      Serial.println();
+      Serial.println("Saved data successfully");
+    }else{
+      Serial.println();
+      Serial.println("Failed:" + fbdo.errorReason());
+    }
+  }
+}
+
+
